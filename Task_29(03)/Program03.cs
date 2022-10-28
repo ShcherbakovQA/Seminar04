c16be7e4035228fa0909fde722608e82f0c26142
Alexander <alex17.05.84@yandex.ru>
2022-10-28 18:17:31 +0300

    Task 29 done

---
+++ Task_29(03)/Program03.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29: Напишите программу, которая задаёт 
+// массив из 8 элементов и выводит их на экран.
+
+int HaveNumber()
+{
+    Console.WriteLine("Введите число обозначающее длину массива");
+    return Convert.ToInt32(Console.ReadLine());
+}
+void MakeArray(int a)
+{
+    int[] array = new int[a];
+    for (int i = 0; i < a; i++)
+    {
+        if (i == a-1)
+        {
+          array[i] = new Random().Next(0,21);
+        Console.Write(array[i]); 
+        Console.WriteLine();
+        break;
+        }
+
+        array[i] = new Random().Next(0,21);
+        Console.Write(array[i] + ",");
+    }
+}
+MakeArray(HaveNumber());
